8ef56c681e1fefd22d7e1fe4d1288f087940fe44
Syrnnik <sakh.alexandr@gmail.com>
2021-06-24 05:46:03 +1100

    Add commands: mute, kick, ban.

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-# Good BOT
+# Good Bot
 Bot description
 
 
 ## Commands
 All commands starts with **prefix**.  
 You can change **Prefix** using command `prefix`.
-Default **prefix** -   
-Example: `/kick` *member* *reason*
+Default **prefix** - !  
+Command using example: `/kick` *member* *reason*
 
-- ### `mute` *member* *reason*
-Mute user on server for a while.  
-**Время** по умолчанию - *10 минут*
+---
+
+- ### `mute` *member* *time* *reason*
+**Mute** user on server for a while.  
+Default **time** - *10 minutes*
 
 - ### `kick` *member* *reason*
-Kick user from server.
+**Kick** user from server.  
 
 - ### `ban` *member* *reason*
-Ban user on server.
+**Ban** user on server.  
--- index.js
+++ index.js
@@ -17,7 +17,90 @@ bot.on('message', (msg) => {
     // Message from this or other bot
     if (msg.author.bot || msg.author == bot.user) return
 
+    // Necessary roles
+    muteRole = msg.guild.roles.cache.find(r => r.name.toLowerCase().startsWith("mute"))
+    // Role position under bot role(because permissions)
+    pos = msg.guild.roles.cache.find(r => r.name == bot.user.username).position
+    // Create mute role
+    if (!muteRole) {
+        msg.guild.roles.create({
+            data: {
+                name: "Muted",
+                color: 'GRAY',
+            }
+        }).setPosition(pos-1)
+    }
+    // Move mute role to top
+    if (muteRole.position != pos-1) {
+        muteRole.setPosition(pos-1)
+    }
 
+    // Mute user on server
+    if (msg.content.startsWith(prefix + "mute")) {
+
+        member = msg.mentions.members
+        // Some member was mentioned
+        if (!member || member.size == 0 || typeof (member) != "object") return msg.channel.send(`You need to mention member in command.`)
+        // Was mentioned more than 1 members
+        else if (member.size > 1) return msg.channel.send(`You need to mention only one member in command.`)
+        member = member.first()
+
+        time = Number(msg.content.split(' ')[2])
+        // Set default time
+        if (!time || typeof (time) != "number") time = 10
+
+        reason = msg.content.split(' ')[3]
+        // Some informative reason
+        if (!reason) reason = `Because ${msg.member.displayName} wants.`
+
+        // Check user for necessary permissions
+        if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('MANAGE_ROLES')) {
+            console.log(`${member.displayName} was muted by ${msg.member.displayName} for ${time} minutes.`)
+            //! member.roles.add(muteRole)
+        }
+    }
+
+    // Kick user from server
+    if (msg.content.startsWith(prefix + "kick")) {
+
+        member = msg.mentions.members
+        // Some member was mentioned
+        if (!member || member.size == 0 || typeof (member) != "object") return msg.channel.send(`You need to mention member in command.`)
+        // Was mentioned more than 1 members
+        else if (member.size > 1) return msg.channel.send(`You need to mention only one member in command.`)
+        member = member.first()
+
+        reason = msg.content.split(' ')[3]
+        // Some informative reason
+        if (!reason) reason = `Because ${msg.member.displayName} wants.`
+
+        // Check user for necessary permissions
+        if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('KICK_MEMBERS')) {
+            console.log(`${member.displayName} was kicked by ${msg.member.displayName}.`)
+            //! member.kick(reason)
+        }
+    }
+
+    // Ban user from server
+    if (msg.content.startsWith(prefix + "ban")) {
+
+        member = msg.mentions.members
+        // Some member was mentioned
+        if (!member || member.size == 0 || typeof (member) != "object") return msg.channel.send(`You need to mention member in command.`)
+        // Was mentioned more than 1 members
+        else if (member.size > 1) return msg.channel.send(`You need to mention only one member in command.`)
+        member = member.first()
+
+        reason = msg.content.split(' ')[3]
+        // Some informative reason
+        if (!reason) reason = `Because ${msg.member.displayName} wants.`
+
+        // Check user for necessary permissions
+        if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('BAN_MEMBERS')) {
+            console.log(`${member.displayName} was banned by ${msg.member.displayName}.`)
+            //! member.ban(reason)
+        }
+    }
 
 })
 
