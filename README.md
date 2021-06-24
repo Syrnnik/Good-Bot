d2dba79f199d795961cbf2a7320d14b1b5599531
Syrnnik <sakh.alexandr@gmail.com>
2021-06-24 23:48:00 +1100

    Add unmute command.

--- README.md
+++ README.md
@@ -4,35 +4,22 @@ Discord Bot for server administration.
 ## General commands
 All commands starts with **prefix**. Default **prefix** - `/`  
 
-<<<<<<< Updated upstream
-## Mute user on server for a while
-#### `mute` `<member>` `<time>` `[reason]`
-- **`<member>`** - **member mention** which user will be muted.  
-- **`<time>`** - **time** for which user will be muted. Default **time** - *10 minutes*  
-- **`[reason]`** - **reason** why user was muted. Default **reason** - *"Because \<message author\> wants."*
-
-- #### `kick` `<member>` `[reason]` - kick user from server.  
-	- **`<member>`** - **member mention** which user will be kicked.  
-	- **`[reason]`** - **reason** why user was kicked. Default **reason** - *"Because \<message author\> wants."*
-
-- #### `ban` `<member>` `[reason]`
-	> Ban user on server.  
-    - **`<member>`** - **member mention** which user will be muted.  
-	- **`[reason]`** - **reason** why user was banned. Default **reason** - *"Because \<message author\> wants."*
-=======
 ## Mute member on server for a while
 #### `/mute` `<member>` `<time>` `"[reason]"`
-- **`<member>`** - **member mention** which member will be muted.  
-- **`<time>`** - how long member will be muted. Default **time** - *10 minutes*  
-- **`[reason]`** - **reason** why member was muted. Default **reason** - *"Because \<message author\> wants."*
+- **`<member>`** - **member mention** which **member** will be muted.  
+- **`<time>`** - how long **member** will be muted. Default **time** - *10 minutes*  
+- **`[reason]`** - **reason** why **member** was muted. Default **reason** - *"Because \<message author\> wants."*
+
+## Unmute member on server for a while
+#### `/unmute` `<member>`
+- **`<member>`** - **member mention** which **member** will be unmuted.
 
 ## Kick member from server
 #### `/kick` `<member>` `"[reason]"`
-- **`<member>`** - **member mention** which member will be kicked.  
-- **`[reason]`** - **reason** why member was kicked. Default **reason** - *"Because \<message author\> wants."*
+- **`<member>`** - **member mention** which **member** will be kicked.  
+- **`[reason]`** - **reason** why **member** was kicked. Default **reason** - *"Because \<message author\> wants."*
 
 ## Ban member on server
 #### `/ban` `<member>` `"[reason]"`
-- **`<member>`** - **member mention** which member will be banned.  
-- **`[reason]`** - **reason** why member was banned. Default **reason** - *"Because \<message author\> wants."*
->>>>>>> Stashed changes
+- **`<member>`** - **member mention** which **member** will be banned.  
+- **`[reason]`** - **reason** why **member** was banned. Default **reason** - *"Because \<message author\> wants."*
--- index.js
+++ index.js
@@ -53,15 +53,35 @@ bot.on('message', (msg) => {
 
         reason = msg.content.split('"')[1]
         // Some informative reason
-        if (!reason) reason = `Because ${msg.member.displayName} wants.`
+        if (!reason) reason = `Because ${msg.member} wants.`
 
         // Check user for necessary permissions
-        if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('MANAGE_ROLES')) {
-            console.log(`${member.displayName} was muted by ${msg.member.displayName} for ${time} minutes.\nReason: ${reason}`)
+        if (msg.member.hasPermission('ADMINISTRATOR')/* || msg.member.hasPermission('MANAGE_ROLES')*/) {
+            console.log(`${member.displayName} was muted by ${msg.member.displayName} for ${time} minutes.`)
             member.roles.add(muteRole)
-            msg.channel.send(`${member.displayName} was muted by ${msg.member.displayName} for ${time} minutes.\nReason: ${reason}`)
+            setTimeout(() => member.roles.remove(muteRole), time*60*1000)
+            msg.channel.send(`${member} was muted by ${msg.member} for ${time} minutes.\nReason: ${reason}`)
         }
     }
+
+    // Unmute user on server
+    if (msg.content.startsWith(prefix + "unmute")) {
+
+        member = msg.mentions.members
+        // Some member was mentioned
+        if (!member || member.size == 0 || typeof (member) != 'object') return msg.channel.send(`You need to mention member in command.`)
+        // Was mentioned more than 1 members
+        else if (member.size > 1) return msg.channel.send(`You need to mention only one member in command.`)
+        member = member.first()
+
+        // Check user for necessary permissions
+        if (msg.member.hasPermission('ADMINISTRATOR')/* || msg.member.hasPermission('MANAGE_ROLES') */) {
+            console.log(`${member.displayName} was unmuted by ${msg.member.displayName}.`)
+            member.roles.remove(muteRole)
+            msg.channel.send(`${member} was unmuted by ${msg.member}.`)
+        }
+    }
+
 
     // Kick user from server
     if (msg.content.startsWith(prefix + "kick")) {
@@ -75,15 +95,16 @@ bot.on('message', (msg) => {
 
         reason = msg.content.split('"')[1]
         // Some informative reason
-        if (!reason) reason = `Because ${msg.member.displayName} wants.`
+        if (!reason) reason = `Because ${msg.member} wants.`
 
         // Check user for necessary permissions
         if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('KICK_MEMBERS')) {
-            console.log(`${member.displayName} was kicked by ${msg.member.displayName}.\nReason: ${reason}`)
+            console.log(`${member.displayName} was kicked by ${msg.member.displayName}.`)
             member.kick(reason)
-            msg.channel.send(`${member.displayName} was kicked by ${msg.member.displayName}.\nReason: ${reason}`)
+            msg.channel.send(`${member} was kicked by ${msg.member}.\nReason: ${reason}.`)
         }
     }
+
 
     // Ban user from server
     if (msg.content.startsWith(prefix + "ban")) {
@@ -95,15 +116,19 @@ bot.on('message', (msg) => {
         else if (member.size > 1) return msg.channel.send(`You need to mention only one member in command.`)
         member = member.first()
 
+        time = Number(msg.content.split(' ')[2])
+        // Set default time
+        if (!time || typeof (time) != 'number') time = 0
+
         reason = msg.content.split('"')[1]
         // Some informative reason
-        if (!reason) reason = `Because ${msg.member.displayName} wants.`
+        if (!reason) reason = `Because ${msg.member} wants.`
 
         // Check user for necessary permissions
         if (msg.member.hasPermission('ADMINISTRATOR') || msg.member.hasPermission('BAN_MEMBERS')) {
-            console.log(`${member.displayName} was banned by ${msg.member.displayName}.\nReason: ${reason}`)
-            member.ban(reason)
-            msg.channel.send(`${member.displayName} was banned by ${msg.member.displayName}.\nReason: ${reason}`)
+            console.log(`${member.displayName} was banned by ${msg.member.displayName}.`)
+            member.ban({ days: time, reason: reason })
+            msg.channel.send(`${member} was banned by ${msg.member}.\nReason: ${reason}.`)
         }
     }
 
